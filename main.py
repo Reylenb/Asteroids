b9537c5e1fe1038d9da94b89ccf8d4af1d4a8864
Reylen <samukai007@gmail.com>
2025-03-31 21:56:25 +0200

    Créer le joueur q

--- main.py
+++ main.py
@@ -4,22 +4,38 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 
 def main():
     pygame.init()
-    print('Starting Asteroids!')
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
-    display.flip()
+
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
